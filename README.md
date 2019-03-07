c2fbe1674df47554d92fd28ee21c9ed902e1994f
lutfioz <luttfioz@gmail.com>
2019-03-07 20:50:02 +0300

    Add init app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotnet-demo
+
+ln -s /usr/local/share/dotnet/dotnet /usr/local/bin/
+
+dotnet new console -o myApp  
+dotnet run  
+
---
+++ myApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //XmlSerializer xml = new XmlSerializer();
+            //FileStream xmlStream = new FileStream("Patch/To/File.xml", FileMode.Open);
+            //var result = xml.Deserialize(xmlStream);
+        }
+    }
+}
